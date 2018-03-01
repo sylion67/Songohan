db1c985db3807f56f036659339dee4be1107fa91
sylion67 <mngs.jean@gmail.com>
2018-03-01 16:06:43 +0100

    Ajout taille police

--- Apps/article.php
+++ Apps/article.php
@@ -1,17 +1,17 @@
 <?php
-if (isset($_GET['id']))
-{// http://localhost/index.php?page=article&id=2
-	$id = $_GET['id'];
-	$article = $manager->find($id);
-	//$sql = "SELECT * FROM `articles` WHERE id=".$id;
-	//$res = mysqli_query($db, $sql);
-	if ($article)
-	{
-		require('views/article.phtml');
-	}
-	else
-	{
-		$error = "/!\  L'article ".$id." n'existe pas /!\ ";
-		require('apps/error.php');
-	}
-}
+// if (isset($_GET['id']))
+// {// http://localhost/index.php?page=article&id=2
+// 	$id = $_GET['id'];
+// 	$article = $manager->find($id);
+// 	//$sql = "SELECT * FROM `articles` WHERE id=".$id;
+// 	//$res = mysqli_query($db, $sql);
+// 	if ($article)
+// 	{
+		require('Views/article.phtml');
+	// }
+	// else
+	// {
+	// 	$error = "/!\  L'article ".$id." n'existe pas /!\ ";
+	// 	require('apps/error.php');
+	// }
+// }
