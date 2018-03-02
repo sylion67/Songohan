b2690faa33ea4a7c173c677b36742e48ebe646fe
sylion67 <mngs.jean@gmail.com>
2018-03-02 15:58:01 +0100

    Ajout de plein de choses

--- Apps/resumeorder.php
+++ Apps/resumeorder.php
@@ -1,2 +1,5 @@
 <?php
-require('Views/resumeorder.php');
+$manager = new ProductsManager($pdo);
+$products = $manager->findById("2");
+var_dump($products);
+require('Views/resumeorder.phtml');
--- Models/Order.class.php
+++ Models/Order.class.php
@@ -1,6 +1,34 @@
 <?php
 class Order
 {
+	private $order_Number;
+	private $order_Date;
+	private $customer_id;
+	private $pdo;
 
+	public function __construct($pdo)
+	{
+		$this->pdo = $pdo;
+	}
+
+	public function getOrderNumber()
+	{
+		return $this->order_Number;
+	}
+
+	public function getOrderDate()
+	{
+		return $this->order_Date;
+	}
+
+	public function getCustomerId()
+	{
+		return $this->customer_id;
+	}
+
+	public function setCustomerId()
+	{
+		$this->customer_id = $customer_id;
+	}
 }
 ?>
