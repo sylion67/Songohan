eb04bb7ee4352493bd2b8eb1df3a6ab09b227ba8
sylion67 <mngs.jean@gmail.com>
2018-03-02 10:29:02 +0100

    Ajout contenu dans main.js

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,1 +1,12 @@
 'Use strict';
+
+/*$(".btn").addClass("btn-active");
+
+$(".btn").removeClass("btn-active");
+
+if ( $(".btn").hasClass("btn-active") )*/
+
+$('.btn').click(function() { 
+  $('.btn').removeClass('btn-active'); 
+  $(this).addClass('btn-active'); 
+});
