8e79a8c4ccaae7f51d7ebb1c68e22cbd11c7c66e
sylion67 <mngs.jean@gmail.com>
2018-03-05 13:42:39 +0100

    Ajout testorder

--- Models/OrderManager.class.php
+++ Models/OrderManager.class.php
@@ -50,12 +50,12 @@ class OrderManager
 		$query = $this->pdo->prepare("UPDATE orders SET order_number=?, order_date=?, customer_id=? WHERE order_number=?");
 		$query->execute([$order->getOrderNumber(), $order->getOrderDate(), $order->getCustomerId()]);
 		$products = $order->getProducts();
-		$query = $this->pdo->prepare("DELETE FROM orderdetails WHERE order_number=?");
-		$query->prepare([$order->getOrderNumber()])
-		$query = $this->pdo->prepare("INSERT INTO orderdetails (order_Number, products_id) VALUES(?, ?)");
-		while ()
+		$query_reset = $this->pdo->prepare("DELETE FROM orderdetails WHERE order_number=?");
+		$query_reset->prepare([$order->getOrderNumber()]);
+		$query_save = $this->pdo->prepare("INSERT INTO orderdetails (order_Number, products_id) VALUES(?, ?)");
+		foreach ($order->getProducts() AS $product)
 		{
-			$query->execute([$order->getOrderNumber(), $products->getProducts()->getProductId()]);
+			$query_save->execute([$order->getOrderNumber(), $product->getProductId()]);
 		}
 		return $this->find($order->getOrderNumber());
 	}
