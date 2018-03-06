3d0877cf84a65606a3b91d05cdbbb0aa5ea0a5ab
sylion67 <mngs.jean@gmail.com>
2018-03-06 11:53:45 +0100

    Modif lien bdd

--- Apps/menu.php
+++ Apps/menu.php
@@ -1,2 +1,3 @@
 <?php
+
 require('Views/menu.phtml');
---
+++ Apps/traitement/order.php
@@ -0,0 +1,47 @@
+<?php
+if (isset($_POST['action']))
+{
+	$manager = new OrderManager($pdo);
+	$action = $_POST['action'];
+	if ($action == 'create')
+	{
+		if (isset($_POST['customer_id']))
+		{
+			$customer = $_POST['customer_id'];
+			$order = $manager->create($customer);
+			// $sql = "INSERT INTO orders (title, content, picture, author) VALUES('".$title."', '".$content."', '".$picture."', '".$author."')";
+			// mysqli_query($db, $sql);
+			header('Location: index.php?page=orderadmin&id='.$order->getOrderNumber());
+			exit;
+		}
+	}
+	else if ($action == 'edit')
+	{
+		if (isset($_POST['customer_id'] ))
+		{
+			$customer = $_POST['customer_id'];
+			$id = $_POST['id'];
+			$order = $manager->find($id);
+			$manager->save($order);
+			//$sql = "UPDATE orders SET title='".$title."', content='".$content."', picture='".$picture."', author='".$author."' WHERE id=".$id;
+			//mysqli_query($db, $sql);
+			header('Location: index.php?page=orderadmin&id='.$order->getOrderNumber());
+			exit;
+		}
+	}
+	else if ($action == 'delete')
+	{
+		if (isset($_POST['id']))
+		{
+			$id = $_POST['id'];
+			$order = $manager->find($id);
+			$manager->remove($order);
+			//$sql = "DELETE FROM orders WHERE id=".$id;
+			//mysqli_query($db, $sql);
+			header('Location: index.php?page=listeorder');
+			exit;
+		}
+	}
+}
+
+?>
