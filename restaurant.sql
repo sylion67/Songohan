d5083541e171438b19fddf6ffe4b01f809666742
sylion67 <mngs.jean@gmail.com>
2018-03-07 16:41:57 +0100

    send nudes

--- restaurant.sql
+++ restaurant.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 06 Mars 2018 à 16:36
+-- Généré le :  Mer 07 Mars 2018 à 16:38
 -- Version du serveur :  5.7.20-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
@@ -108,7 +108,14 @@ CREATE TABLE `customers` (
 INSERT INTO `customers` (`customer_id`, `customer_FirstName`, `customer_LastName`, `phone`, `addressLine`, `city`, `postalCode`, `email`) VALUES
 (1, 'Bulma', 'Magali', '1122334455', '14 rue de la Boule de cristal', 'Namek', 52361, 'bubu@gmail.com'),
 (2, 'Chichi', 'Kadija', '1122334455', '18 rue de Shenron', 'Terre', 52361, 'chichi@gmail.com'),
-(3, 'Sangoku', 'Jean', '1122334455', '3 rue du Super Saiyan', 'Planète Vegeta', 52361, 'sangoku@gmail.com');
+(3, 'Sangoku', 'Jean', '1122334455', '3 rue du Super Saiyan', 'Planète Vegeta', 52361, 'sangoku@gmail.com'),
+(4, 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 1324, 'aaa@aaa.aaa'),
+(5, 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 1324, 'aaa@aaa.aaa'),
+(6, 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 1324, 'aaa@aaa.aaa'),
+(7, 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 1324, 'aaa@aaa.aaa'),
+(8, 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 1324, 'aaa@aaa.aaa'),
+(9, 'aaa', 'aaa', 'aaa', 'aaa', 'aaa', 1234, 'aaa@aaa.aaa'),
+(10, 'aaa', 'aaa', '00000000', '14 rue des ergolmdkf', 'fmshljghlksdfj', 1234, 'pdo@truc.com');
 
 -- --------------------------------------------------------
 
@@ -133,7 +140,19 @@ INSERT INTO `orderdetails` (`id`, `order_Number`, `product_id`) VALUES
 (4, 2, 4),
 (5, 1, 1),
 (6, 1, 2),
-(7, 1, 2);
+(7, 1, 2),
+(8, 3, 6),
+(9, 3, 11),
+(10, 3, 11),
+(11, 3, 1),
+(12, 3, 1),
+(13, 3, 1),
+(14, 3, 1),
+(15, 4, 4),
+(16, 4, 4),
+(17, 4, 4),
+(18, 4, 6),
+(19, 4, 6);
 
 -- --------------------------------------------------------
 
@@ -153,7 +172,9 @@ CREATE TABLE `orders` (
 
 INSERT INTO `orders` (`order_Number`, `order_Date`, `customer_id`) VALUES
 (1, '2018-02-28 19:27:26', 1),
-(2, '2018-02-28 19:27:26', 2);
+(2, '2018-02-28 19:27:26', 2),
+(3, '2018-03-07 14:52:24', 9),
+(4, '2018-03-07 15:20:07', 10);
 
 -- --------------------------------------------------------
 
@@ -175,19 +196,18 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_title`, `product_description`, `price`, `picture`, `type`) VALUES
-(1, 'Coca', 'Coca cola la boisson qui vous raffraichira lors de vos grands repas japonais', 2, 'Public/img/menu/coca.jpg', 'boisson'),
-(2, 'Pizza', 'Pizza fromages avec son fromage mousseux.', 4, 'Public/img/menu/plat3.jpg', 'plat'),
-(3, 'Boule de coco', 'Vous voulez bien finir vos repas ? Voici le dessert parfait qui saura ravir vos papilles.', 1.5, 'Public/img/menu/plat1.jpg', 'dessert'),
-(4, 'Salade composé', 'Délicieuse salade composé, excellent mélange,  assaisonné avec une vinaigrette classique.', 12, 'Public/img/menu/entree2.jpg', 'entre'),
-(5, 'Yakitori', 'Délicieux plat à base de crevette accompagner de sa boule de cristal farcie à la viande Hachée que sa sauce à la tomate fraiche', 10, 'Public/img/menu/plat2.jpg', 'plat'),
-(6, 'Super salade', 'Super salade de rouleaux de printemps enroulé dans une galette fine de riz aux sept boules de cristal.', 12, 'Public/img/menu/entree3.jpg', 'entre'),
-(7, 'Bouboule de glaçe', 'Un bon dessert à base de meringue et glaçe à la vanille', 9, 'Public/img/menu/dessert3.jpg', 'dessert'),
-(8, 'Boisson énergétique', 'Boisson énergétique Japonaise, au goût pouvant se définir comme une sorte de saveur d\'orange.', 5, 'Public/img/menu/dbz.jpg', 'boisson'),
-(9, 'Café', 'Café léger avec son nuage de crème, soupoudrer de cacao. ', 5, 'Public/img/menu/boisson.jpg', 'boisson'),
-(10, 'Dessert dans les nuages', 'Dessert aux fruits avec son nuage flottant.', 12, 'Public/img/menu/dessert1.jpg', 'dessert'),
-(11, 'Bao farcie à la châtaigne', 'Délicieux pain moelleux farcie aux chataignes.', 12, 'Public/img/menu/entree1.jpg', 'entre'),
-(12, 'Bateauuuuuu', 'Voilà un beau bateau', 40, 'Public/img/menu/plat4.jpg', 'plat'),
-(13, 'Test', 'Test', 20, 'Public/img/menu/dessert3.jpg', 'plat');
+(2, 'Flying Nimbus', 'Pizza aux 4 fromages, surmontée d\'un nuage de coton aromatisé au citron', 13, 'Public/img/menu/plat3.jpg', 'plat'),
+(3, 'Quatre étoiles', 'Dragon Ball à la mangue, coeur mousse au chocolat blanc, étoiles framboise. Déposée sur un croustillant au chocolat', 9, 'Public/img/menu/dessert2.jpg', 'dessert'),
+(4, 'Salade Piccolo', 'Retirez le sceau sacré du bocal et dégustez une savoureuse salade composée aux couleurs de Piccolo...', 9, 'Public/img/menu/entree2.jpg', 'entre'),
+(5, 'Goku Stars', 'Savourez un riz frit enrobé, sauce chili, légumes frais et crevettes. Aux étoiles de paprika\r\n', 15, 'Public/img/menu/plat2.jpg', 'plat'),
+(6, 'Salade Shenron', 'Invoquez le dragon avec ces 7 boules de cristal ! Rouleaux de printemps aux crevettes, légumes frais, poulet cuit à la vapeur et sauce chili douce.\r\n', 9.5, 'Public/img/menu/entree3.jpg', 'entre'),
+(7, 'BooBoo de glace', 'Meringue au goût de marshmallow, crème glacée à la vanille, éclats de chocolat, fruits rouges et crème anglaise\r\n', 7.5, 'Public/img/menu/dessert3.jpg', 'dessert'),
+(9, 'Cappuccino', 'Retrouvez le portrait de votre personnage favoris sur un nuage de crème....', 4, 'Public/img/menu/boisson.jpg', 'boisson'),
+(10, 'Nuage flottant', 'Nuage de coton sur sa crête moelleuse, déposés sur un lit d\'agrumes', 9.5, 'Public/img/menu/dessert1.jpg', 'dessert'),
+(11, 'Bao Kuririn', 'Bao cuites à la vapeur, fourrées aux châtaignes et au boeuf', 5.5, 'Public/img/menu/entree1.jpg', 'entre'),
+(19, 'Dragon Ball Burger', 'Buns aux pigments naturels de citrouille, aux étoiles de paprika rouge. \r\nSteak, légumes frais, fromage fondu et fruit du dragon pour sublimer le goût.\r\n', 11.5, 'Public/img/menu/plat1.jpg', 'plat'),
+(20, 'Fusion', 'Unissez ces 2 boissons et revivez la fusion ! \r\nAu sirop de melon, étoiles de coco, curaçao bleu gazeux', 5, 'Public/img/menu/boisson1.jpg', 'boisson'),
+(21, 'Super Saiyan', 'Boisson énergisante qui vous transformera en réel saiyan !', 2.5, 'Public/img/menu/boisson2.jpg', 'boisson');
 
 -- --------------------------------------------------------
 
@@ -210,7 +230,10 @@ CREATE TABLE `reservations` (
 INSERT INTO `reservations` (`reservation_id`, `customer_id`, `reservation_date`, `reservation_hour`, `client_number`) VALUES
 (1, 1, '02/03/2018', '18:30', 4),
 (2, 2, '12/03/2018', '19:30', 6),
-(3, 3, '22/03/2018', '21:00', 2);
+(3, 3, '22/03/2018', '21:00', 2),
+(4, 6, '2018-03-08', '20:30', 8),
+(5, 7, '2018-03-08', '20:30', 8),
+(6, 8, '2018-03-08', '20:30', 8);
 
 --
 -- Index pour les tables exportées
@@ -289,27 +312,27 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT pour la table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `orderdetails`
 --
 ALTER TABLE `orderdetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 --
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_Number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `order_Number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 --
 -- AUTO_INCREMENT pour la table `reservations`
 --
 ALTER TABLE `reservations`
-  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `reservation_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
